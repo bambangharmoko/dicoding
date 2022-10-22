eea5cea7b2935a4066affad9f41cca59fd711671
mzkki <08harismu@gmail.com>
2022-10-22 22:13:32 +0800

    KUMPUL SUDAH NJENG

--- Projects/file.js
+++ Projects/file.js
@@ -1,2 +1,23 @@
-// const d = new Date();
-// document.getElementById("demo").innerHTML = d;
+function displayTime() {
+  const date = new Date()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+  const session = document.getElementById('session')
+
+  if (hour >= 12) {
+    session.innerHTML = 'PM'
+  } else {
+    session.innerHTML = 'AM'
+  }
+
+  document.getElementById('hours').innerHTML = hour
+  document.getElementById('minutes').innerHTML = minute
+  document.getElementById('seconds').innerHTML = second
+
+  // console.log(hour)
+  // console.log(minute)
+  // console.log(second)
+}
+setInterval(displayTime, 1000)
+// setInterval(displayTime, 10)
